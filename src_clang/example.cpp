5ca021644ca078cca39f856021fefa8aa73b74ad
Ryan Whelan <rwhelan@ll.mit.edu>
2015-03-11 16:12:23 -0400

    Builds and works for me (Clang 3.6 Release).

--- src_clang/example.cpp
+++ src_clang/example.cpp
@@ -42,7 +42,7 @@ public:
             if (!(funcBody = dyn_cast<CompoundStmt>(f->getBody())))
                     return true;
 
-            SourceLocation loc = funcBody->body_front()->getLocStart();
+            SourceLocation loc = (*funcBody->body_begin())->getLocStart();
             rewriter.InsertText(loc, query.str(), true, true);
         }
   
