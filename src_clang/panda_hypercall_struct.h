b55e7adc960fdd6f41e66e7d3a8674b5f2bf08aa
Brendan Dolan-Gavitt <brendan@cs.columbia.edu>
2015-04-16 16:37:01 -0400

    Change hypercall yet again. Seems to defeat optimizer

--- src_clang/panda_hypercall_struct.h
+++ src_clang/panda_hypercall_struct.h
@@ -7,16 +7,19 @@
  * Keep me in sync between PANDA and LAVA repos
  */
 
+#pragma pack(push,1)
 typedef struct panda_hypercall_struct {
-  unsigned long long action;      // label / query / etc
-  unsigned long long buf;         // ptr to memory we want labeled or queried or ...
+  unsigned int magic;
+  unsigned long int action;      // label / query / etc
+  unsigned long int buf;         // ptr to memory we want labeled or queried or ...
   unsigned long len;         // number of bytes to label or query or ...
   unsigned long label_num;   // if labeling, this is the label number.  if querying this should be zero
   //  uint32_t offset;      // offset is used for what?
-  unsigned long long src_filename;  // if querying from src this is a char * to filename.  
-  unsigned long long src_linenum;   // if querying from src this is the line number
-  unsigned long long src_ast_node_name;     // if querying from src this is the name of the l-value queries 
+  unsigned long int src_filename;  // if querying from src this is a char * to filename.  
+  unsigned long int src_linenum;   // if querying from src this is the line number
+  unsigned long int src_ast_node_name;     // if querying from src this is the name of the l-value queries 
 } PandaHypercallStruct;
+#pragma pack(pop)
 
 #endif
 
