2c2ed128408986fdd958a09e9670c6b8014a2d2f
Ryan Whelan <rwhelan@ll.mit.edu>
2015-04-16 18:09:14 -0400

    New panda_hypercall_struct.

--- src_clang/panda_hypercall_struct.h
+++ src_clang/panda_hypercall_struct.h
@@ -7,17 +7,24 @@
  * Keep me in sync between PANDA and LAVA repos
  */
 
+#ifdef PANDA
+#include "stdint.h"
+typedef uint32_t lavaint;
+#else
+typedef unsigned int lavaint;
+#endif
+
 #pragma pack(push,1)
 typedef struct panda_hypercall_struct {
-  unsigned int magic;
-  unsigned long int action;      // label / query / etc
-  unsigned long int buf;         // ptr to memory we want labeled or queried or ...
-  unsigned long len;         // number of bytes to label or query or ...
-  unsigned long label_num;   // if labeling, this is the label number.  if querying this should be zero
-  //  uint32_t offset;      // offset is used for what?
-  unsigned long int src_filename;  // if querying from src this is a char * to filename.  
-  unsigned long int src_linenum;   // if querying from src this is the line number
-  unsigned long int src_ast_node_name;     // if querying from src this is the name of the l-value queries 
+  lavaint magic;
+  lavaint action;             // label / query / etc
+  lavaint buf;                // ptr to memory we want labeled or queried or ...
+  lavaint len;                // number of bytes to label or query or ...
+  lavaint label_num;          // if labeling, this is the label number.  if querying this should be zero
+  lavaint src_column;         // column on source line
+  lavaint src_filename;       // char * to filename.  
+  lavaint src_linenum;        // line number
+  lavaint src_ast_node_name;  // the name of the l-value queries 
 } PandaHypercallStruct;
 #pragma pack(pop)
 
