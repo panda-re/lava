db9c96a1a25fa00409017f2e224b2ae95a0bb79f
Ryan Whelan <rwhelan@ll.mit.edu>
2015-04-17 14:55:44 -0400

    Update file script.

--- scripts/fileLavaScript.sh
+++ scripts/fileLavaScript.sh
@@ -8,11 +8,12 @@ cp -r file-5.11-orig file-5.11
 # Assumes you've gathered and saved original compile_commands.json
 cp file_compile_commands_complete.json file-5.11/compile_commands.json
 cd file-5.11
-cp ~/git/lava/src_clang/pirate_mark_lava.h src/
-cp ~/git/lava/src_clang/panda_hypercall_struct.h src/
+cp ~/git/lava/include/pirate_mark_lava.h src/
+cp ~/git/lava/include/panda_hypercall_struct.h src/
 ./configure --prefix=`pwd`/install
 /home/$USER/git/lava/src_clang/get_c_files.py /home/$USER/src/file-5.11 | \
-    while read line; do /home/$USER/git/lava/src_clang/build/lavaTaintQueryTool -p=/home/$USER/src/file-5.11 $line; done
+    while read line; do /home/$USER/git/lava/src_clang/build/lavaTool \
+    -lava-db=/home/$USER/src/file-5.11/lavadb.db -p=/home/$USER/src/file-5.11 $line; done
 make -j $(nproc)
 make install
 cd ..
