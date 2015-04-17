7adb4bd5eaee1c311a41f0df3a138768551a57bd
Tim Leek <tleek@ll.mit.edu>
2015-04-16 21:27:57 -0400

    added info an reindented

--- include/panda_hypercall_struct.h
+++ include/panda_hypercall_struct.h
@@ -16,15 +16,16 @@ typedef unsigned int lavaint;
 
 #pragma pack(push,1)
 typedef struct panda_hypercall_struct {
-  lavaint magic;
-  lavaint action;             // label / query / etc
-  lavaint buf;                // ptr to memory we want labeled or queried or ...
-  lavaint len;                // number of bytes to label or query or ...
-  lavaint label_num;          // if labeling, this is the label number.  if querying this should be zero
-  lavaint src_column;         // column on source line
-  lavaint src_filename;       // char * to filename.  
-  lavaint src_linenum;        // line number
-  lavaint src_ast_node_name;  // the name of the l-value queries 
+    lavaint magic;
+    lavaint action;             // label / query / etc
+    lavaint buf;                // ptr to memory we want labeled or queried or ...
+    lavaint len;                // number of bytes to label or query or ...
+    lavaint label_num;          // if labeling, this is the label number.  if querying this should be zero
+    lavaint src_column;         // column on source line
+    lavaint src_filename;       // char * to filename.  
+    lavaint src_linenum;        // line number
+    lavaint src_ast_node_name;  // the name of the l-value queries 
+    lavaint info;               // general info
 } PandaHypercallStruct;
 #pragma pack(pop)
 
