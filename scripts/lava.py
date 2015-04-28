1605d70a6a0b953bfa3ec36e1bca22c0da6ebc83
Ryan Whelan <rwhelan@ll.mit.edu>
2015-04-28 18:12:44 -0400

    Some progress on parsing LAVA files and invoking bug insertion.  Not quite usable yet.

--- scripts/lava.py
+++ scripts/lava.py
@@ -22,7 +22,7 @@
     ===============
 
     Some example insertion examples from Amy:
-    build/lavaTool -action=inject -mode=atp inputs/test_input.c -info=20,memcpy,dead_data1,4 --
+    build/lavaTool -action=inject -mode=atp inputs/test_input.c -info=20,memcpy,dead_data1,4 -- (atp info is currently: line, type, new global name, size)
     build/lavaTool -p=/home/amy/src/binutils-2.22 -action=inject -mode=dua $source -info=1906,abfd,[112,113,114,115,],dead_data1
 '''
 
@@ -39,22 +39,86 @@ def parseConfigFile(configFilePath):
 
 
 def lavaInsertTaintQueries(config):
-    ''' Pull stuff in from, and generalize fileLavaScript.sh '''
+    ''' Pull stuff in from, and generalize fileLavaScript.sh and get_c_files.py '''
     print 'Inserting taint queries...'
+
+
+def parseAPFile(config):
+    ''' Parse the attack points file '''
+    attackPoints = []
+    with open(config['lavaDataPath'] + '/lava.aps') as apFile:
+        for line in apFile:
+            ap = line.strip('\n').split(',')
+            attackPoints.append(dict([
+                ('fileName', ap[0]),
+                ('lineNumber', ap[1]),
+                ('astNode', ap[2])
+            ]))
+    return attackPoints
+
+
+def parseDUAFile(config):
+    ''' Parse the DUAs file '''
+    return None
+
+
+def insertAttackPointTransforms(config, attackPoints):
+    ''' Call lavaTool with data read from a .aps file '''
+    for i, ap in enumerate(attackPoints):
+        spArgs = [
+            config['toolPath'] + '/lavaTool',
+            '-p=' + config['sourcePath'],
+            '-action=inject',
+            '-mode=atp',
+            ('-info=' + ap['lineNumber'] + ',' +
+                ap['astNode'] + ',' +
+                'dead_data1,4' # placeholder global and size
+            ),
+            ap['fileName']
+        ]
+        print 'spArgs: ' + str(spArgs)
+        proc = subprocess.Popen(spArgs)
+        proc.wait()
+        if proc.returncode != 0:
+            print ('Error returned from subprocess ' + spArgs[0] +
+                ' on file ' + ap['fileName'] + '...')
+            sys.exit(1)
+
+
+def insertDuaTransforms(config, attackPoints):
+    ''' Call lavaTool with data read from a .aps file '''
+    return None
 
 
 def lavaInsertBugs(config):
     print 'Inserting bugs...'
-    spArgs = [config['toolPath'] + 'lavaTool', '-action=inject']
-    proc = subprocess.Popen(spArgs)
-    proc.wait()
-    if proc.returncode != 0:
-        print 'Error returned from subprocess ' + spArgs[0] + '...'
-        sys.exit(1)
+    print '\tTransforming attack points in .aps file...\n\n'
+    aps = parseAPFile(config)
+    insertAttackPointTransforms(config, aps)
+
+    #print '\tTransforming DUAs in .duas file...'
+    duas = parseDUAFile(config)
+    insertDuaTransforms(config, duas)
+    print '\n\nDone inserting bugs.'
+
+
+def compileSource(config):
+    ''' Compile a source project, original or transformed.  Confirm that
+    compilation completed without errors.
+    '''
+    return None
+
+
+def runProgram(config, crashExpected):
+    ''' Run a program with arguments specified in the configuration file.
+    Confirm expected behavior of a successful bug (crash), or successful
+    transformation (no crash).
+    '''
+    return None
 
 
 def getPandaRecording():
-    '''Restore to a booted snapshot, mount an ISO with transformed
+    ''' Restore to a booted snapshot, mount an ISO with transformed
     source/compiled binaries, begin recording, execute a parameterized command,
     end recording
     '''
@@ -62,8 +126,7 @@ def getPandaRecording():
 
 
 def runPandaReplay():
-    '''Run a replay of an instrumented binary to get LAVA information
-    '''
+    ''' Run a replay of an instrumented binary to get LAVA information '''
     return None
 
 
@@ -76,7 +139,6 @@ def main():
     args = ap.parse_args()
 
     config = parseConfigFile(args.config)
-    print 'Source path:' + config['sourcePath']
 
     if args.action == 'insert':
         lavaInsertBugs(config)
