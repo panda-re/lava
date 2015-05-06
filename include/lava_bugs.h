16d6f62afd93603681ff23480a185662f46bff7e
Tim Leek <tleek@ll.mit.edu>
2015-05-06 10:16:29 -0400

    unmarshalling dua,atp,and bug tables from postgres via cpp all work

--- include/lava_bugs.h
+++ include/lava_bugs.h
@@ -2,18 +2,21 @@
 #ifndef __LAVA_BUGS_H__
 #define __LAVA_BUGS_H__
 
-#include <stdint.h>
+extern "C" {
+    #include <stdlib.h>
+    #include <stdint.h>
+    #include "/usr/include/postgresql/libpq-fe.h"
+}
 
+
+#include <string>
+#include <sstream>
+#include <map>
 #include <set>
 
-// byte offset within lval extent queried for taint
-typedef uint32_t Offset;
+
 // instruction count
 typedef uint64_t Instr;
-// taint label (input byte #)
-typedef uint32_t Label;
-// line number
-typedef uint32_t Line;
 // Taint Compute Number
 typedef uint32_t Tcn;
 // ptr used to ref a label set
@@ -21,50 +24,50 @@ typedef uint64_t Ptr;
 
 
 
-
 std::string iset_str(std::set<uint32_t> &iset);
 
 
 
-class Dua {    
+struct Dua {    
 
-public:    
-    std::string filename;        // name of src file this dua is in
-    Line line;                   // line in that src file
-    std::string lvalname;        // name of the lval, i.e. x or y->f or x[i].m or ...
-    std::set<Label>labels;       // byte offsets within input file that taint the dua parts of this lval
-    std::set<Offset>bytes;       // offsets within this lval that are dua
-    std::string input_file;      // name of input file used to discover this dua
-    float max_liveness;          // max liveness of any label in any taint set associated with a dua byte of this lval
-    uint32_t max_tcn;            // max tcn of any byte of this lval
-    uint32_t max_card;           // max cardinality of any taint set for any byte of this lval
-    uint32_t icount;             // num times this dua has been used to inject a bug
-    uint32_t scount;             // num times this dua has been used to inject a bug that turned out to realy be a bug
+    std::string filename;               // name of src file this dua is in
+    uint32_t line;                          // line in that src file
+    std::string lvalname;               // name of the lval, i.e. x or y->f or x[i].m or ...
+    std::set<uint32_t> file_offsets;  // byte offsets within input file that taint the dua parts of this lval
+    std::set<uint32_t> lval_offsets;  // offsets within this lval that are dua
+    std::string input_file;             // name of input file used to discover this dua
+    float max_liveness;                 // max liveness of any label in any taint set for dua
+    uint32_t max_tcn;                   // max tcn of any byte of this lval
+    uint32_t max_card;                  // max cardinality of any taint set for any byte of this lval
+    uint32_t icount;                    // num times this dua has been used to inject a bug
+    uint32_t scount;                    // num times this dua has been used to inject a bug that turned out to realy be a bug
 
+    /*
     Dua (std::string filename,        
-         Line line,                   
+         uint32_t line,                   
          std::string lvalname,        
-         std::set<Label>labels,       
-         std::set<Offset>bytes,       
+         std::set<uint32_t> file_offsets,       
+         std::set<uint32_t> lval_offsets,       
          std::string input_file,      
          float max_liveness,          
          uint32_t max_tcn,            
-         uint32_t max_card)
-        : filename(filename), line(line), lvalname(lvalname), labels(labels),
-        bytes(bytes), input_file(input_file), max_liveness(max_liveness),
-        max_tcn(max_tcn), max_card(max_card) {}    
-       
+         uint32_t max_card,
+         uint32_t icount,
+         uint32_t scount)
+        : filename(filename), line(line), lvalname(lvalname), file_offsets(file_offsets), lval_offsets(lval_offsets), input_file(input_file), max_liveness(max_liveness),  max_tcn(max_tcn), max_card(max_card), icount(icount), scount(scount) {}    
+    */
+
     std::string str() {
-        std::stringstream crap1;
-        crap1 << filename << ","
+        std::stringstream ss;
+        ss << filename << ","
               << line << ","  
               << lvalname << ",";
         // offsets within the input file that taint dua
-        crap1 << "{" << iset_str(labels) << "},"; 
+        ss << "{" << iset_str(file_offsets) << "},"; 
         // offsets within the lval that are duas
-        crap1 << "{" << iset_str(bytes) << "},";
-        crap1 << "," << max_liveness << "," << max_tcn << "," << max_card ;
-        return crap1.str();
+        ss << "{" << iset_str(lval_offsets) << "}";
+        ss << "," << max_liveness << "," << max_tcn << "," << max_card ;
+        return ss.str();
     }    
 
     bool operator<(const Dua &other) const {
@@ -86,9 +89,9 @@ public:
         if (icount > other.icount) return false;
         if (scount < other.scount) return true;
         if (scount > other.scount) return false;
-        if (labels < other.labels) return true;
-        if (labels > other.labels) return false;
-        return bytes < other.bytes;
+        if (file_offsets < other.file_offsets) return true;
+        if (file_offsets > other.file_offsets) return false;
+        return lval_offsets < other.lval_offsets;
     }
 
     bool operator==(const Dua &other) const {
@@ -101,28 +104,37 @@ public:
                 && (max_card == other.max_card) 
                 && (icount == other.icount) 
                 && (scount == other.scount) 
-                && (labels == other.labels) 
-                && (bytes == other.bytes));
+                && (file_offsets == other.file_offsets) 
+                && (lval_offsets == other.lval_offsets));
     }
 
 };
 
 
 
-class AttackPoint {
+struct AttackPoint {
 
-public:
     std::string filename;   // src filename this attack point is in
-    Line linenum;              // line number in that file
+    uint32_t linenum;              // line number in that file
     std::string typ;        // name of type of attack point, i.e. "memcpy", or "malloc"
-    
-    AttackPoint (std::string filename, uint32_t linenum, std::string typ) 
-        : filename(filename), linenum(linenum), typ(typ) {}
+    std::string input_file; 
+    uint32_t icount;
+    uint32_t scount;
+
+    /*    
+    AttackPoint (std::string filename,
+                 uint32_t linenum,
+                 std::string typ, 
+                 std::string input_file,
+                 uint32_t icount,
+                 uint32_t scount)
+        : filename(filename), linenum(linenum), typ(typ), input_file(input_file), icount(icount), scount(scount) {}
+    */
 
     std::string str() {
-        std::stringstream crap1;
-        crap1 << filename << "," << linenum << "," << typ;
-        return crap1.str();
+        std::stringstream ss;
+        ss << filename << "," << linenum << "," << typ << "," << input_file << "," << icount << "," << scount;
+        return ss.str();
     }
 
     bool operator<(const AttackPoint &other) const {
@@ -130,43 +142,53 @@ public:
         if (linenum > other.linenum) return false;
         if (filename < other.filename) return true;
         if (filename > other.filename) return false;
-        return (typ < other.typ);
+        if (typ < other.typ) return true;
+        if (typ > other.typ) return false;
+        if (input_file < other.input_file) return true;
+        if (input_file > other.input_file) return false;
+        if (icount < other.icount) return true;
+        if (icount > other.icount) return false;
+        return (scount < other.scount);
     }
 
     bool operator==(const AttackPoint &other) const {
         return ((linenum == other.linenum) 
                 && (filename == other.filename) 
-                && (typ == other.typ));
+                && (typ == other.typ)
+                && (input_file == other.input_file)
+                && (icount == other.icount)
+                && (scount == other.scount));
     }
-
+    
 };
 
 
-class Bug {
-
-public:
+struct Bug {
 
     Dua dua;                 // specifies where the dead data is in the program src
     AttackPoint atp;         // specifes where that dead data might be used to create a bug
     std::string global_name;  // name of global that will provide data flow between dua and atp.
     uint32_t size;            // size of global in bytes
-    
+
+    /*    
     Bug (Dua dua, AttackPoint atp, std::string global_name, uint32_t size) 
         : dua(dua), atp(atp), global_name(global_name), size(size) {}
-    
+    */
+
     std::string str() {
-        std::stringstream crap1;
-        crap1 << "Dua{" << dua.str() << "}-{"<< atp.str() << "}," << global_name << "," << size;
-        return crap1.str();
+        std::stringstream ss;
+        ss << "Dua{" << dua.str() << "}-Atp{" << atp.str() << "}," << global_name << "," << size;
+        return ss.str();
     }
     
     bool operator<(const Bug &other) const {
+        // NB: got no > for dua & atp. make do with < & ==
         if (dua < other.dua) return true;
         if (!(dua == other.dua)) return false; // must be >
         if (atp < other.atp) return true;
         if (!(atp == other.atp)) return false;
         if (global_name < other.global_name) return true;
-        if (!(global_name == other.global_name)) return false;
+        if (global_name > other.global_name) return false;
         return (size < other.size);
     }
 
@@ -176,9 +198,27 @@ public:
             && (global_name == other.global_name)  
             && (size == other.size));
     }
-
     
 };
+
+
+PGconn *pg_connect(void);
+
+void exit_nicely(PGconn *conn);
+
+// execute query on db via postgresql connection conn
+PGresult *pg_exec(PGconn *conn, std::string query);
+
+// samle but uses the more convenient stringstream
+PGresult *pg_exec_ss(PGconn *conn, std::stringstream &query);
+
+/*
+ assumes table named tablename is in the postgresql db.
+ that table has two columns.  first is integer ids.
+ second is strings corresponding to thos ids.  
+ this fn reads that info into a int->string map and returns it
+*/
+std::map<uint32_t,std::string> pq_get_string_map(PGconn *conn, std::string tablename);
 
 
 
