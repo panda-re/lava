f517cfb7d7d5f02fcb6f62cb34d7d6a8f97f5bcb
Patrick Hulin <patrick.hulin@gmail.com>
2015-07-22 11:38:52 -0400

    Queries.sh now performs btrace and adds queries.

--- automate/queries.sh
+++ automate/queries.sh
@@ -19,7 +19,7 @@ lava="$(dirname $(dirname $(realpath $0)))"
 directory="$(jq -r .directory $json)"
 name="$(jq -r .name $json)"
 
-progress "Entering $directory/$name"...
+progress "Entering $directory/$name."
 mkdir -p "$directory/$name"
 cd "$directory/$name"
 
@@ -28,8 +28,22 @@ tarfile="$(jq -r .tarfile $json)"
 progress "Untarring $tarfile..."
 tar xf "$(jq -r .tarfile $json)"
 source=$(ls)
+
+progress "Entering $source."
+cd "$source"
+
+progress "Configuring..."
+$(jq -r .configure $json)
+
+progress "Making..."
+$lava/btrace/sw-btrace $(jq -r .make $json)
+
+progress "Creating compile_commands.json..."
+$lava/btrace/sw-btrace-to-compiledb /home/moyix/git/llvm/Debug+Asserts/lib/clang/3.6.1/include
+
+cd ..
 c_files=$(python $lava/src_clang/get_c_files.py $source)
-c_dirs=$(dirname $c_files | sort | uniq)
+c_dirs=$(for i in $c_files; do dirname $i; done | sort | uniq)
 
 progress "Copying include files..."
 for i in $c_dirs; do
@@ -38,6 +52,9 @@ for i in $c_dirs; do
 done
 
 progress "Inserting queries..."
-$lava/src_clang/build/lavaTool
+for i in $c_files; do
+  $lava/src_clang/build/lavaTool -action=query -lava-db=lavadb \
+    -p="$source/compile_commands.json" "$i"
+done
 
 progress "Done inserting queries. Time to make and run automate."
