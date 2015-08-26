5e21c2ec10d26987907589bc7630e47324ad6a0d
Tim Leek <tleek@ll.mit.edu>
2015-08-26 12:17:32 -0400

    catching ssh return value and also now create db if it doesnt exist

--- scripts/everything.sh
+++ scripts/everything.sh
@@ -30,12 +30,15 @@
 # testinghost: what host to test injected bugs on
 # 
 
+#set -e # Exit on error                                                                                                                                                                                          
 
-set -e # Exit on error                                                                                                                                                                                          
+
 if [ $# -lt 1 ]; then
   echo "Usage: $0 JSONfile "
   exit 1
 fi      
+
+
 
 progress() {
   echo  
@@ -48,18 +51,23 @@ progress() {
 num_inject=1
 skip_to_inject=0
 ok=0
+skip_deleting=0
 
 # -s means skip everything up to injection
 # -i 15 means inject 15 bugs (default is 1)
 echo 
 progress "Parsing args"
-while getopts  "si:k" flag
+while getopts  "sni:k" flag
 do
 #  echo "$flag $OPTARG"
   if [ "$flag" = "s" ]; then
     skip_to_inject=1
     progress "-s: Skipping ahead to inject bugs"
   fi  
+  if [ "$flag" = "n" ]; then
+    skip_deleting=1
+    progress "-s: Skipping deleting of directories and dbs"
+  fi    
   if [ "$flag" = "i" ]; then
     num_inject=$OPTARG
     progress "-i: num_inject = $num_inject"
@@ -71,6 +79,7 @@ do
 done
 shift $((OPTIND -1))
 
+json="$(realpath $1)"
 
 
 deldir () {
@@ -98,11 +107,15 @@ run_remote() {
   command=$2
   echo "ssh $remote_machine $command"
   ssh $remote_machine $command
+  ret_code=$?
+    echo "exit code was $ret_code"
+  if [ $ret_code != 0 ]; then
+    echo "exit code was $ret_code"
+  fi
+  return $ret_code
 }
-
         
 
-json="$(realpath $1)"
 
 progress "JSON file is $json"
 
@@ -127,23 +140,40 @@ logs="$directory/$name/logs"
 
 if [ $skip_to_inject -eq 0 ]; then
 
-    deldir "$sourcedir"
-    deldir "$logs"
-    deldir "$bugsdir"
+    dbexists=$(psql -tAc "SELECT 1 from pg_database where datname='$db'" -U postgres)
+    echo "dbexists $dbexists"
+
+    if [ -z $dbexists ]; then
+        progress "No db -- creating $db"
+        run_remote "$dbhost" "createdb -U postgres $db"
+    else
+        if [ $dbexists -eq 1 ]; then
+            progress "database $db already exists"
+        else 
+            progess "wtf"
+            exit 1111
+        fi
+    fi
+
+    if [ $skip_deleting -eq 0 ]; then
+        deldir "$sourcedir"
+        deldir "$logs"
+        deldir "$bugsdir"
+        /bin/mkdir -p $logs
+        lf="$logs/dbwipe.log"  
+        progress "Wiping db $db & setting up anew -- logging to $lf"
+        run_remote "$dbhost" "/usr/bin/psql -d $db -f $lava/sql/lava.sql -U postgres >& $lf"
+    fi
     /bin/mkdir -p $logs
 
-    lf="$logs/dbwipe.log"  
-    progress "Wiping db $db -- logging to $lf"
-    run_remote "$dbhost" "/usr/bin/psql -d $db -f $lava/sql/lava.sql -U postgres >& $lf"
-    
     lf="$logs/add_queries.log" 
     progress "Adding queries to source -- logging to $lf"
     run_remote "$buildhost" "$scripts/add_queries.sh $json >& $lf" 
     
     lf="$logs/make.log"
     progress "Making 32-bit version with queries -- logging to $lf"
-    run_remote "$buildhost" "cd $sourcedir && make -j `nproc` >& $lf"
-    run_remote "$buildhost" "cd $sourcedir && make install &>> $lf"
+    run_remote "$buildhost" "cd $sourcedir && make -j `nproc`  >& $lf"
+    run_remote "$buildhost" "cd $sourcedir && make install   &>> $lf"
     
     for input in $inputs
     do
@@ -157,6 +187,8 @@ if [ $skip_to_inject -eq 0 ]; then
 else
     progress "Skipping ahead to injection"
 fi
+
+/bin/mkdir -p $logs
 
 progress "Injecting $num_inject bugs"
 for i in `seq $num_inject`
