5b4ceebd8c9b294f4d77708ebdc2f1f124c52f25
Tim Leek <tleek@tintin.mit.edu>
2015-11-13 12:27:42 -0500

    more writing

--- paper1/inj-example-dua-siphon.c
+++ paper1/inj-example-dua-siphon.c
@@ -3,13 +3,14 @@ file_encoding(struct magic_set *ms,
               ..., const char **type) {
 ...
    else if
-     (({int rv = looks_extended(buf, nbytes, *ubuf, ulen);
+     (({int rv = 
+          looks_extended(buf, nbytes, *ubuf, ulen);
        if (buf) {
          int lava = 0;
-         lava |= ((unsigned char *) (buf))[0] << (0*8);
-         lava |= ((unsigned char *) (buf))[1] << (1*8);
-         lava |= ((unsigned char *) (buf))[2] << (2*8);
-         lava |= ((unsigned char *) (buf))[3] << (3*8);
+         lava |= ((unsigned char *)(buf))[0]<<(0*8);
+         lava |= ((unsigned char *)(buf))[1]<<(1*8);
+         lava |= ((unsigned char *)(buf))[2]<<(2*8);
+         lava |= ((unsigned char *)(buf))[3]<<(3*8);
          lava_set(lava);
        }; rv;})) {
 ...
