77506fa522e6b13d827483eee4abea94494a8149
Patrick Hulin <patrick.hulin@gmail.com>
2016-12-06 15:47:56 -0500

    Remove gcc-4.9 dependency and update to llvm-3.6.2.

--- scripts/get-clang.sh
+++ scripts/get-clang.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 #release="3.8.0"
-release="3.6.1"
+release="3.6.2"
 llvm_version="llvm-$release"
-
-##### FIRST INSTALL GCC 4.9 #####
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get -y install gcc-4.9 g++-4.9
-#################################
 
 wget http://llvm.org/releases/$release/$llvm_version.src.tar.xz
 tar -xJf $llvm_version.src.tar.xz
 
 mv $llvm_version.src $llvm_version
 cd $llvm_version
+
 pushd tools
 
 clang_version="cfe-$release"
@@ -22,14 +17,16 @@ wget http://llvm.org/releases/$release/$clang_version.src.tar.xz
 tar -xJf $clang_version.src.tar.xz
 mv $clang_version.src clang
 
-cd clang/tools
+pushd clang/tools
 wget http://llvm.org/releases/$release/clang-tools-extra-$release.src.tar.xz
 tar -xJf clang-tools-extra-$release.src.tar.xz
 mv clang-tools-extra-$release.src extra
+popd
 
 popd
-CC=`which gcc-4.9` CXX=`which g++-4.9` ./configure
-make -j
+
+./configure --enable-optimized --disable-assertions --enable-targets=x86,arm --enable-shared --enable-pic --host=x86_64-linux-gnu --build=x86_64-linux-gnu
+REQUIRES_RTTI=1 make -j $(nproc)
 #mkdir $llvm_version-build
 #cd $llvm_version-build
 #$cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../$llvm_version
