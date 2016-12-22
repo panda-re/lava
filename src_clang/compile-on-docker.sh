5f025dddb2a5687b2d93884f6075b73d2a869f1b
Patrick Hulin <patrick.hulin@gmail.com>
2016-12-22 11:33:24 -0500

    Update compile-on-docker to use your user.

--- src_clang/compile-on-docker.sh
+++ src_clang/compile-on-docker.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+set -x
 
-LAVA_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )")"
+lava="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )")"
 
-docker run \
-    --rm -it -v $LAVA_DIR:$LAVA_DIR lava32 \
-    make -C $LAVA_DIR/src_clang -j${PANDA_NPROC:-$(nproc || sysctl -n hw.ncpu)}
+docker run --rm -it \
+    -e "HTTP_PROXY=$HTTP_PROXY" \
+    -e "HTTPS_PROXY=$HTTPS_PROXY" \
+    -e "http_proxy=$http_proxy" \
+    -e "https_proxy=$https_proxy" \
+    -v /var/run/postgresql:/var/run/postgresql \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -v /etc/shadow:/etc/shadow:ro \
+    -v /etc/gshadow:/etc/gshadow:ro \
+    -v "$lava":"$lava" \
+    lava32 sh -c "trap '' PIPE; su -l phulin -c 'make -j$(nproc) -C \"$lava\"/src_clang'"
