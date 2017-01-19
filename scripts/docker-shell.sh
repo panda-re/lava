8adff2097a91bd171e926c43fd9e2118f208961c
Patrick Hulin <patrick.hulin@gmail.com>
2017-01-19 14:25:26 -0500

    Update docker-shell to use persistent container.
    Also fix locale issue.

--- scripts/docker-shell.sh
+++ scripts/docker-shell.sh
@@ -20,24 +20,21 @@ docker_map_args="-v $tarfiledir:$tarfiledir"
 if [[ "$directory" = "$tarfiledir"* ]]; then true; else
   docker_map_args="$docker_map_args -v $directory:$directory"
 fi
-echo docker run --rm -it \
-    -e "HTTP_PROXY=$HTTP_PROXY" \
-    -e "HTTPS_PROXY=$HTTPS_PROXY" \
-    -e "http_proxy=$http_proxy" \
-    -e "https_proxy=$https_proxy" \
-    -v /var/run/postgresql:/var/run/postgresql \
-    -v /etc/passwd:/etc/passwd:ro \
-    -v /etc/group:/etc/group:ro \
-    -v /etc/shadow:/etc/shadow:ro \
-    -v /etc/gshadow:/etc/gshadow:ro \
-    -v "$HOME":"$HOME" \
-    $docker_map_args \
-    lava32 sh -c "apt-get install sudo gdb; trap '' PIPE; login -f $(whoami)"
+
+if ! ( docker images lava32debug | grep -q lava32debug ); then
+    docker build -t lava32debug "$(dirname $(dirname $(readlink -f $0)))/docker/debug"
+fi
+
+whoami="$(whoami)"
+set +x
 docker run --rm -it \
     -e "HTTP_PROXY=$HTTP_PROXY" \
     -e "HTTPS_PROXY=$HTTPS_PROXY" \
     -e "http_proxy=$http_proxy" \
     -e "https_proxy=$https_proxy" \
+    -e "LANG=en_US.UTF-8" \
+    -e "LANGUAGE=en_US:en" \
+    -e "LC_ALL=en_US.UTF-8" \
     -v /var/run/postgresql:/var/run/postgresql \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
@@ -45,4 +42,4 @@ docker run --rm -it \
     -v /etc/gshadow:/etc/gshadow:ro \
     -v "$HOME":"$HOME" \
     $docker_map_args \
-    lava32 sh -c "apt-get install -y sudo gdb; trap '' PIPE; login -f $(whoami)"
+    lava32debug sh -c "trap '' PIPE; login -f $whoami LANG=en_US.UTF-8 LANGUAGE=en_US LC_ALL=en_US.UTF-8"
