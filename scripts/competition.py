447b17855caf9e1b66d2396f005b16106c5d04ba
Tim Leek <tleek@ll.mit.edu>
2017-02-21 15:43:56 -0500

    not working still hacking

--- scripts/competition.py
+++ scripts/competition.py
@@ -101,7 +101,6 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--bugdir', action="store", default=False,
             help = 'directory for corpus')
     
-
     args = parser.parse_args()
     project = json.load(args.project)
     project_file = args.project.name
@@ -111,9 +110,6 @@ if __name__ == "__main__":
 
     db = LavaDatabase(project)
 
-    timeout = project.get('timeout', 5)
-    
-#
     try:
         os.makedirs(args.bugdir)
     except Exception: pass
@@ -126,26 +122,24 @@ if __name__ == "__main__":
     print (str(lp))
     
 
-#    if args.buglist:
-#        bug_list = eval(args.buglist)
-#    elif args.many:
-#        bug_list = competition_bugs_and_non_bugs(int(args.many), db)
+    if False:
+        if args.buglist:
+            bug_list = eval(args.buglist)
+        elif args.many:
+            bug_list = competition_bugs_and_non_bugs(int(args.many), db)
 
-    bug_list = [22364, 451089, 543885, 556072]
+    bug_list = [475187L, 135879L, 442734L, 555117L, 85147L, 55982L, 232105L, 353204L, 423770L, 278537L]
 
-    knobTrigger = False
+    knobTrigger = -1
 
     # add all those bugs to the source code and check that it compiles
     (build, input_files) = inject_bugs(bug_list, bugs_parent, db, lp, project_file, \
-                                          project, -1, False)
-
+                                          project, knobTrigger, False)
 
     # determine which of those bugs actually cause a seg fault
     real_bug_list = validate_bugs(bug_list, db, lp, project, input_files, build, \
-                                      -1, False, timeout)
+                                      knobTrigger, False)
 
-
-    
     if len(real_bug_list) == 0:
         print "Oops we don't have any real bugs?  Try again"
         sys.exit(0)
@@ -154,76 +148,68 @@ if __name__ == "__main__":
     (build,input_files) = inject_bugs(real_bug_list, bugs_parent + "_real", db, lp, project_file, \
                                           project, -1, False)
 
+    # determine which of those bugs actually cause a seg fault
+    real2_bug_list = validate_bugs(real_bug_list, db, lp, project, input_files, build, \
+                                       knobTrigger, False)
+    
+    corpus_dir = lp.bugs_top_dir + "/corpora"
+    subprocess32.check_call(["mkdir", "-p", corpus_dir])
 
-
-#    # only makes sense to try to package a corpus if we are injecting several bugs.
-#    if args.corpus:
-#        assert (args.many)
-#        corpus_dir = join(lp.top_dir, "corpus")
-
-
-
-
-
-
-    if 1==0 and args.corpus:
-        # package up a corpus
-        subprocess32.check_call(["mkdir", "-p", corpus_dir])
-        print "created corpus dir " + corpus_dir + "\n"
-        # original bugs src dir
-        bd = join(bugs_parent, source_root)
-        # directory for this corpus
-        corpname = "lava-corpus-" + ((datetime.datetime.now()).strftime("%Y-%m-%d-%H-%M-%S"))
-        corpdir = join(corpus_dir,corpname)
-        subprocess32.check_call(["mkdir", corpdir])
-        # subdir with trigger inputs
-        inputsdir = join(corpdir, "inputs")
-        subprocess32.check_call(["mkdir", inputsdir])
-        # subdir with src -- note we can't create it or copytree will fail!
-        srcdir = join(corpdir, "src")
-        # copy src
-        shutil.copytree(bd, srcdir)
-        # copy over the inputs as well
-        predictions = {}
-        for bug_index, bug in enumerate(bugs_to_inject):
-            if not (bug.id in real_bugs):
-                continue
-            print "validating bug %d" % bug.id
-            # make sure this bug actually works and
-            # triggers at the attack point as expected
-            if (check_bug(bug.id, sys.argv[-1], project['lava'] + "/scripts/run-on-fuzzed-input.py")):
-                print "  -- works and triggers in the right place"
-                prediction = "{}:{}".format(basename(bug.atp.loc_filename),
-                                            get_atp_line(bug, bugs_build))
-                print prediction
-                if prediction in predictions:
-                    print "... but we already have a bug for that attack point"
-                else:
-                    fuzzed_input = "{}-fuzzed-{}{}".format(pref, bug.id, suff)
-                    (dc, fi) = os.path.split(fuzzed_input)
-                    shutil.copy(fuzzed_input, inputsdir)
-                    predictions[prediction] = fi
+    print "created corpus dir " + corpus_dir + "\n"
+    # original bugs src dir
+    bd = join(lp.bugs_parent, lp.source_root)
+    # directory for this corpus
+    corpname = "lava-corpus-" + ((datetime.datetime.now()).strftime("%Y-%m-%d-%H-%M-%S"))
+    corpdir = join(corpus_dir,corpname)
+    subprocess32.check_call(["mkdir", corpdir])
+    # subdir with trigger inputs
+    inputsdir = join(corpdir, "inputs")
+    subprocess32.check_call(["mkdir", inputsdir])
+    # subdir with src -- note we can't create it or copytree will fail!
+    srcdir = join(corpdir, "src")
+    # copy src
+    shutil.copytree(bd, srcdir)
+    # copy over the inputs as well
+    predictions = {}
+    for bug_index, bug in enumerate(bugs_to_inject):
+        if not (bug.id in real_bugs):
+            continue
+        print "validating bug %d" % bug.id
+        # make sure this bug actually works and
+        # triggers at the attack point as expected
+        if (check_bug(bug.id, sys.argv[-1], project['lava'] + "/scripts/run-on-fuzzed-input.py")):
+            print "  -- works and triggers in the right place"
+            prediction = "{}:{}".format(basename(bug.atp.loc_filename),
+                                        get_atp_line(bug, bugs_build))
+            print prediction
+            if prediction in predictions:
+                print "... but we already have a bug for that attack point"
             else:
-                print "  -- either doesnt work or triggers in wrong place"
-        print "Answer key:"
-        ans = open(join(corpdir, "ans"), "w")
-        for prediction in predictions:
-            print "ANSWER  [%s] [%s]" % (prediction, predictions[prediction])
-            ans.write("%s %s\n" % (prediction, predictions[prediction]))
-        ans.close()
-        # clean up before tar
-        os.chdir(srcdir)
-        subprocess32.check_call(["make", "distclean"])
-        shutil.rmtree(join(srcdir, ".git"))
-        shutil.rmtree(join(srcdir, "lava-install"))
-        os.remove(join(srcdir, "compile_commands.json"))
-        os.remove(join(srcdir, "btrace.log"))
-        tarball = join(srcdir + ".tgz")
-        os.chdir(corpdir)
-        cmd = "/bin/tar czvf " + tarball + " src"
-        subprocess32.check_call(cmd.split())
-        print "created corpus tarball " + tarball + "\n";
-        build = open(join(corpdir, "build"), "w")
-        build.write("%s\n" % project['configure'])
-        build.write("%s\n" % project['make'])
-        build.close()
+                fuzzed_input = "{}-fuzzed-{}{}".format(pref, bug.id, suff)
+                (dc, fi) = os.path.split(fuzzed_input)
+                shutil.copy(fuzzed_input, inputsdir)
+                predictions[prediction] = fi
+        else:
+            print "  -- either doesnt work or triggers in wrong place"
+    print "Answer key:"
+    ans = open(join(corpdir, "ans"), "w")
+    for prediction in predictions:
+        print "ANSWER  [%s] [%s]" % (prediction, predictions[prediction])
+        ans.write("%s %s\n" % (prediction, predictions[prediction]))
+    ans.close()
+    # clean up before tar
+    os.chdir(srcdir)
+    subprocess32.check_call(["make", "distclean"])
+    shutil.rmtree(join(srcdir, ".git"))
+    shutil.rmtree(join(srcdir, "lava-install"))
+    os.remove(join(srcdir, "compile_commands.json"))
+    os.remove(join(srcdir, "btrace.log"))
+    tarball = join(srcdir + ".tgz")
+    os.chdir(corpdir)
+    cmd = "/bin/tar czvf " + tarball + " src"
+    subprocess32.check_call(cmd.split())
+    print "created corpus tarball " + tarball + "\n";
+    build = open(join(corpdir, "build"), "w")
+    build.write("%s\n" % project['configure'])
+    build.write("%s\n" % project['make'])
+    build.close()
