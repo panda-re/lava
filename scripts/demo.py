b01ff480d66355883dd32e2bc7edd824f4a84033
Patrick Hulin <patrick.hulin@gmail.com>
2017-03-08 16:31:24 -0800

    Add animated volcano.

--- scripts/demo.py
+++ scripts/demo.py
@@ -8,12 +8,14 @@ import curses
 import subprocess32 as sb
 
 from os.path import basename, join
+from random import random, randrange
+from threading import Event, Lock, Thread
 
 from lava import LavaDatabase, AttackPoint, Bug, Dua, Build, Run, get_suffix
 
-log = open ("/tmp/lava-demo-{}.log".format(getpass.getuser()), "w")
+log = open("/tmp/lava-demo-{}.log".format(getpass.getuser()), "w")
 log_ind = 0
-def logp (msg):
+def logp(msg):
     global log_ind
     log.write("%d: %s\n" % (log_ind, msg))
     log.flush()
@@ -77,12 +79,12 @@ def check_for(pattern, hostfile):
 
 def wait_for(pattern, hostfile):
     while True:
-        if (check_for(pattern, hostfile)):
+        if check_for(pattern, hostfile):
             return
 
 # extracts last
 def extract_float(pattern, hostfile):
-    assert (check_for(pattern, hostfile))
+    assert check_for(pattern, hostfile)
     res = find_in_file_extract(pattern, hostfile)
     return float(res[-1])
 
@@ -90,9 +92,52 @@ def extract_int(pattern, hostfile):
     res = find_in_file_extract(pattern, hostfile)
     return int(res[-1])
 
+def addstr(lock, mon, r, c, s, *args):
+    with lock:
+        for i, line in enumerate(s.splitlines()):
+            mon.addstr(r + i, c, line, *args)
+        mon.refresh()
+
+def addstr_bold(lock, mon, r, c, s):
+    addstr(lock, mon, r, c, s, curses.A_BOLD)
+
+def smoke(origin_r, origin_c, current_smoke):
+    result = []
+    for i, (digit, dr, dc) in enumerate(current_smoke):
+        dr -= 1
+        dc += 0.4 + random() * 0.2
+        if int(dr) > 0 and int(dc) < 79:
+            result.append((digit, dr, dc))
+    if random() < 0.4:
+        result.append((str(randrange(0, 2)), origin_r, origin_c))
+    return result
+
+volcano_str = """\
+        _
+       / \\
+      /  /\\
+     /   \ \\
+    /    /  \\
+   /\   /\   \\
+  /  \  \ \   \\
+ /   /  /  \   \\
+/   /  /   /    \\
+"""
+def volcano(lock, mon, done_event):
+    r, c = 19, 60
+    addstr_bold(lock, mon, r, c, volcano_str)
+    current_smoke = []
+    while not done_event.is_set():
+        for digit, digit_r, digit_c in current_smoke:
+            addstr(lock, mon, int(digit_r), int(digit_c), ' ')
+        current_smoke = smoke(r - 1, c + 8, current_smoke)
+        for digit, digit_r, digit_c in current_smoke:
+            addstr(lock, mon, int(digit_r), int(digit_c), digit)
+        time.sleep(0.2)
+
 def monitor_lava(stdscr):
     curses.curs_set(0)
-    assert (curses.has_colors())
+    assert curses.has_colors()
 
     mon = curses.newwin(30, 80, 4, 4)
     mon.hline(0,1,'-',78)
@@ -100,26 +145,29 @@ def monitor_lava(stdscr):
     mon.vline(1,0,'|',28)
     mon.vline(1,79,'|',28)
 
+    lock = Lock()
+    done_event = Event()
+
     v0=2
-    mon.addstr(v0, 11, "LAVA: Large-scale Automated Vulnerability Addition", curses.A_BOLD)
-    mon.addstr(v0+1, 17, "target: %s" % target_name)
-    mon.refresh()
+    addstr(lock, mon, v0, 11, "LAVA: Large-scale Automated Vulnerability Addition", curses.A_BOLD)
+    addstr(lock, mon, v0+1, 17, "target: %s" % target_name)
+
+    volcano_thread = Thread(target=volcano, args=(lock, mon, done_event))
+    volcano_thread.start()
 
     v1=5
     # stage 1 -- instrument source
     wait_for_file(add_queries_log)
     # ok the add queries log file at least exists
-    mon.addstr(v1+0, 15, "1. Instrument source w/")
-    mon.addstr(v1+1, 15, "   dynamic queries & make")
-    mon.refresh()
+    addstr(lock, mon, v1+0, 15, "1. Instrument source w/")
+    addstr(lock, mon, v1+1, 15, "   dynamic queries & make")
     # get source lines of code
     sb.check_call(["tar", "-xf", project['tarfile'], '-C', '/tmp'])
     outp = sb.check_output(['sloccount', "/tmp/%s" % target_name])
     for line in outp.split("\n"):
         foo = re.search("^ansic:\s+([0-9]+) ", line)
         if foo:
-            mon.addstr(v0+1, 42, "sloc: " + foo.groups()[0])
-            mon.refresh()
+            addstr(lock, mon, v0+1, 42, "sloc: " + foo.groups()[0])
 
     time.sleep(0.1)
 
@@ -139,9 +187,8 @@ def monitor_lava(stdscr):
     natp = 0
     for n in res:
         natp += int(n)
-    mon.addstr(v1, 48,   "taint queries: %d" % ntq)
-    mon.addstr(v1+1, 48, "  atp queries: %d" % natp)
-    mon.refresh()
+    addstr(lock, mon, v1, 48,   "taint queries: %d" % ntq)
+    addstr(lock, mon, v1+1, 48, "  atp queries: %d" % natp)
 
     time.sleep(0.1)
 
@@ -153,33 +200,27 @@ def monitor_lava(stdscr):
 
     tm = extract_float(pattern, make_log)
 
-    mon.addstr(v1, 4, "%4.2fs" % (ti+tm))
-    mon.refresh()
+    addstr(lock, mon, v1, 4, "%4.2fs" % (ti+tm))
 
-#    mon.addstr(9, 4, "%4.2fs" % tm)
-    mon.refresh()
+#    addstr(lock, mon, 9, 4, "%4.2fs" % tm)
     time.sleep(0.1)
 
     # stage 2 -- run instr program & record
     v2=8
     wait_for_file(bug_mining_log)
-    mon.addstr(v2,  15,  "2. Record run of")
-    mon.addstr(v2+1, 15, "   instrumented program")
-    mon.refresh()
-    # wait for record to finish
+    addstr(lock, mon, v2,  15,  "2. Record run of")
+    addstr(lock, mon, v2+1, 15, "   instrumented program")
     pattern = "panda record complete ([0-9\.]+) seconds"
     wait_for(pattern, bug_mining_log)
     tr = extract_float(pattern, bug_mining_log)
-    mon.addstr(v2, 4, "%4.2fs" % tr)
-    mon.refresh()
+    addstr(lock, mon, v2, 4, "%4.2fs" % tr)
 
     # stage 3 -- replay + taint
     v3 = 11
     pattern = "Starting first and only replay"
     wait_for(pattern,bug_mining_log)
-    mon.addstr(v3, 15, "3. Replay with taint")
-    mon.addstr(v3+1, 15, "   propagation")
-    mon.refresh()
+    addstr(lock, mon, v3, 15, "3. Replay with taint")
+    addstr(lock, mon, v3+1, 15, "   propagation")
 
     done = False
     while not done:
@@ -189,41 +230,34 @@ def monitor_lava(stdscr):
         pattern = "([0-9\.]+)\%\) instr"
         if (check_for(pattern, bug_mining_log)):
             perc = extract_float(pattern, bug_mining_log)
-            mon.addstr(v3+1, 35, " %4.2f%%" % perc)
-            mon.refresh()
+            addstr(lock, mon, v3+1, 35, " %4.2f%%" % perc)
         time.sleep(0.11)
-    mon.addstr(v3+1, 35, " 100.00%")
-    mon.refresh()
+    addstr(lock, mon, v3+1, 35, " 100.00%")
     time.sleep(0.11)
-    mon.addstr(v3+1, 35, "        ")
-    mon.refresh()
+    addstr(lock, mon, v3+1, 35, "        ")
 
     # interestiing stats
     pattern = ":\s*([0-9]+) instrs total"
     wait_for(pattern, bug_mining_log)
     ti = extract_int(pattern, bug_mining_log)
-    mon.addstr(v3, 48, "instr: %d" % ti)
-    mon.refresh()
+    addstr(lock, mon, v3, 48, "instr: %d" % ti)
     time.sleep(0.11)
 
     pattern = "taint analysis complete ([0-9\.]+) seconds"
     tt = extract_float(pattern, bug_mining_log)
-    mon.addstr(v3, 4, "%4.2fs" % tt)
-    mon.refresh()
+    addstr(lock, mon, v3, 4, "%4.2fs" % tt)
 
     # figure out how big plog is
     assert os.path.isfile(plog)
     plogsize = os.stat(plog).st_size
-    mon.addstr(v3+1, 48, " plog: %d" % plogsize)
-    mon.refresh()
+    addstr(lock, mon, v3+1, 48, " plog: %d" % plogsize)
 
     time.sleep(0.11)
 
     # stage 4 -- fbi
     v4 = 16
-    mon.addstr(v4, 15,   "4. Analyze taint & find")
-    mon.addstr(v4+1, 15, "   bug inject sites")
-    mon.refresh()
+    addstr(lock, mon, v4, 15,   "4. Analyze taint & find")
+    addstr(lock, mon, v4+1, 15, "   bug inject sites")
     # poll db to find out how many dua and atp we have
 #    first_db = True
     last_num_dua = 0
@@ -238,79 +272,70 @@ def monitor_lava(stdscr):
         num_atp = db.session.query(AttackPoint).count()
         num_bug = db.session.query(Bug).count()
 #        if first_db and (num_dua > 0 or num_atp > 0 or num_bug > 0):
-#            mon.addstr(v4, 48, "Database")
+#            addstr(lock, mon, v4, 48, "Database")
 #            first_db = False
         if num_dua != last_num_dua:
-            mon.addstr(v4, 48, " DUAs: %d" % num_dua)
+            addstr(lock, mon, v4, 48, " DUAs: %d" % num_dua)
         if num_atp != last_num_atp:
-            mon.addstr(v4+1, 48, " ATPs: %d" % num_atp)
+            addstr(lock, mon, v4+1, 48, " ATPs: %d" % num_atp)
         if num_bug != last_num_bug:
-            mon.addstr(v4+2, 48, "pBUGs: %d" % num_bug)
+            addstr(lock, mon, v4+2, 48, "pBUGs: %d" % num_bug)
         last_num_dua = num_dua
         last_num_atp = num_atp
         last_num_bug = num_bug
         time.sleep(0.1)
-        mon.refresh()
 
     tf = extract_float(pattern, bug_mining_log)
-    mon.addstr(v4, 4, "%4.2fs" % tf)
-    mon.refresh()
+    addstr(lock, mon, v4, 4, "%4.2fs" % tf)
 
     # stage 5 inj
     v5=20
-    for trial in range(1, 4):
+    for trial in range(1, 2):
         # inject trial $trial
         lf = join(log_dir, "inject-{}.log".format(trial))
         logp(str(trial))
         wait_for_file(lf)
         if trial == 1:
-            mon.addstr(v5, 15,   "5. Inject bugs &")
-            mon.addstr(v5+1, 15,   "   validate")
+            addstr(lock, mon, v5, 15,   "5. Inject bugs &")
+            addstr(lock, mon, v5+1, 15,   "   validate")
         vt=v5+2+trial
-        mon.addstr(vt, 15, "   trial %d (100 bugs):" % trial)
-        mon.refresh()
+        addstr(lock, mon, vt, 15, "   trial %d (100 bugs):" % trial)
 
         logp("select")
         # select bugs
         pattern = "INJECTING BUGS (.*) SOURCE"
         wait_for(pattern, lf)
-        mon.addstr(vt, 40, "I")
-        mon.refresh()
+        addstr(lock, mon, vt, 40, "I")
 
         logp("compile")
         # compile
         pattern = "ATTEMPTING BUILD (.*) INJECTED BUG"
         wait_for(pattern, lf)
-        mon.addstr(vt, 41, "B")
-        mon.refresh()
+        addstr(lock, mon, vt, 41, "B")
 
         logp("orig")
         # validate -- does orig input still exit with 0?
         pattern = "buggy program succeeds (.*) original input"
         wait_for(pattern, lf)
-        mon.addstr(vt, 42, "O")
-        mon.refresh()
+        addstr(lock, mon, vt, 42, "O")
 
         logp("validate")
         # validate bugs
         pattern = "FUZZED INPUTS"
         check_for(pattern, lf)
         wait_for(pattern, lf)
-        mon.addstr(vt, 43, "V")
-        mon.refresh()
+        addstr(lock, mon, vt, 43, "V")
 
         logp("yield")
         pattern = "yield ([0-9\.]+) \("
         wait_for(pattern, lf)
         y = extract_float(pattern, lf)
-        mon.addstr(vt, 40, "yield: %.2f" % y)
-        mon.refresh()
+        addstr(lock, mon, vt, 40, "yield: %.2f" % y)
 
         pattern = "inject complete ([0-9\.]+) seconds"
         wait_for(pattern, lf)
         ti = extract_float(pattern, lf)
-        mon.addstr(vt, 4, "%.2fs" % ti)
-        mon.refresh()
+        addstr(lock, mon, vt, 4, "%.2fs" % ti)
 
         trial += 1
 
@@ -339,7 +364,9 @@ def monitor_lava(stdscr):
         while True: pass
     except KeyboardInterrupt: pass
 
+    done_event.set()
     try: sb.check_call(['killall', 'sleep'])
     except Exception: pass
+    volcano_thread.join(1)
 
 curses.wrapper(monitor_lava)
