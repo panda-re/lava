64d9e31c49fb4111e4d8a6877811a63b1027a213
hu zhenghao <tonyhu@sjtu.edu.cn>
2017-03-28 01:10:02 -0700

    Explicitly change the visibility of lava_get/lava_set to avoid "hidden symbol `lava_set' in file.o is referenced by DSO" error

--- src_clang/lava_set.c
+++ src_clang/lava_set.c
@@ -1,5 +1,5 @@
 static unsigned int lava_val[1000000] = {0};
 void lava_set(unsigned int bug_num, unsigned int val);
-void lava_set(unsigned int bug_num, unsigned int val) { lava_val[bug_num] = val; }
+__attribute__((visibility("default"))) void lava_set(unsigned int bug_num, unsigned int val) { lava_val[bug_num] = val; }
 unsigned int lava_get(unsigned int bug_num);
-unsigned int lava_get(unsigned int bug_num) { return lava_val[bug_num]; }
+__attribute__((visibility("default"))) unsigned int lava_get(unsigned int bug_num) { return lava_val[bug_num]; }
