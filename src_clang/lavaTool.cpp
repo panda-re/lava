302e5b6311c33c0d1c9e60479d1296fc98fd77f0
Patrick Hulin <patrick.hulin@gmail.com>
2017-04-27 15:59:34 -0400

    Use a list for insertions and deduplicate.

--- src_clang/lavaTool.cpp
+++ src_clang/lavaTool.cpp
@@ -283,26 +283,37 @@ LExpr knobTriggerAttack(const Bug *bug) {
  */
 class Insertions {
 private:
-    std::map<SourceLocation, std::string> impl;
+    // TODO: use map and "beforeness" concept to robustly avoid duplicate
+    // insertions.
+    std::map<SourceLocation, std::list<std::string>> impl;
 
 public:
     void clear() { impl.clear(); }
 
     void InsertAfter(SourceLocation loc, std::string str) {
-        if (!str.empty()) impl[loc].append(str);
+        if (!str.empty()) {
+            std::list<std::string> &strs = impl[loc];
+            if (strs.empty() || strs.back() != str) {
+                impl[loc].push_back(str);
+            }
+        }
     }
 
     void InsertBefore(SourceLocation loc, std::string str) {
         if (!str.empty()) {
-            str.append(impl[loc]);
-            impl[loc] = str;
+            std::list<std::string> &strs = impl[loc];
+            if (strs.empty() || strs.front() != str) {
+                impl[loc].push_front(str);
+            }
         }
     }
 
     void render(const SourceManager &sm, std::vector<Replacement> &out) {
         out.reserve(impl.size() + out.size());
         for (const auto &keyvalue : impl) {
-            out.emplace_back(sm, keyvalue.first, 0, keyvalue.second);
+            std::stringstream ss;
+            for (const std::string &s : keyvalue.second) ss << s;
+            out.emplace_back(sm, keyvalue.first, 0, ss.str());
         }
     }
 };
