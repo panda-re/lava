9560d0e958e8f5c2cf251242770a3484fc452696
Andrea Mambretti <mbr@ccs.neu.edu>
2017-10-23 14:08:13 -0400

    enable multi step transformation during injection

--- scripts/everything.sh
+++ scripts/everything.sh
@@ -297,7 +297,7 @@ if [ $inject -eq 1 ]; then
         if [ "$injfixupsscript" != "null" ]; then
             fix="--fixupsscript='$injfixupsscript'"
         fi
-        run_remote "$testinghost" "$python $scripts/inject.py -m $many -e $exitCode $kt $fix $hostjson $project_name" "$lf"
+        run_remote "$testinghost" "$python $scripts/inject.py -m $many --stage=multi -e $exitCode $kt $fix $hostjson $project_name" "$lf"
     grep yield "$lf"
     done
 fi
