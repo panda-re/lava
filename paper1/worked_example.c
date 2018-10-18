18d6a40bd63556acc6ed4f548fa3f56b7ab6f265
Andrea Mambretti <mbr@ccs.neu.edu>
2018-10-18 16:26:53 -0400

    removed lava paper src becaused moved to a different repo

--- paper1/worked_example.c
+++
@@ -1,9 +0,0 @@
-void foo(int a, int b, char *s, char *d, int n) {
-    int c = a+b;
-    if (a != 0xdeadbeef) 
-        return;  
-    for (int i=0; i<n; i++) 
-        c+=s[i];
-    memcpy(d,s,n+c); // Original source
-    // BUG: memcpy(d+(b==0x6c617661)*b,s,n+c);
-}  
