995c6c3b408f25dfd009a86eeea8edaf0c9be35a
zhenghaohu <huzh@nyu.edu>
2019-11-01 16:31:00 +0000

    final fixup

--- tools/lavaTool/src/lavaTool.cpp
+++ tools/lavaTool/src/lavaTool.cpp
@@ -184,6 +184,8 @@ int main(int argc, const char **argv) {
         }
     }
 
+    std::cout << "Final Real Bug : " << real_bug->id << "\n";
+
     if (t) {
         t->commit();
         delete t;
