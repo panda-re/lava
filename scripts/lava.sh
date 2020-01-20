fb6a696b9d1e3a0ce0e361cfa94d6ad9711f8c90
zhenghaohu <huzh@nyu.edu>
2020-01-20 17:31:14 +0000

    add graphland

--- scripts/lava.sh
+++ scripts/lava.sh
@@ -93,7 +93,7 @@ ATP_TYPE=""
 # default bugtypes
 #bugtypes="ptr_add,rel_write"
 #bugtypes="chaff_bug_stack_unused"
-bugtypes="chaff_bug_stack_const,chaff_bug_heap_const,chaff_bug_stack_unused"
+bugtypes="chaff_bug_stack_const,chaff_bug_heap_const"
 #bugtypes="chaff_bug_heap_const"
 # default # of bugs to be injected at a time
 many=50
