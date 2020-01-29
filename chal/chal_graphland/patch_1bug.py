6791f25918c40bcb2992dacd5597abdf39aa6950
zhenghaohu <huzh@nyu.edu>
2020-01-29 22:30:55 +0000

    add 1bug version of chals

---
+++ chal/chal_graphland/patch_1bug.py
@@ -0,0 +1,20 @@
+import sys, struct
+
+addr = 0x08071112
+offset = 0x3783
+
+with open(sys.argv[1], 'rb') as fd:
+    data = fd.read()
+
+nd = data[:offset] + struct.pack('<I', addr) + data[offset+4:]
+
+system_addr = 0x08070190
+offset = 0x3772 - 0x62
+nd = nd[:offset] + struct.pack('<I', system_addr) + nd[offset+4:]
+
+binsh_addr = 0x080720F0
+offset = 0x3772 - 0x5a
+nd = nd[:offset] + struct.pack('<I', binsh_addr) + nd[offset+4:]
+
+with open('exp_1bug', 'wb') as fd:
+    fd.write(nd)
