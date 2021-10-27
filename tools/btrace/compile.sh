91b23beb511b768e0ab1e43c3c4d6c558c98d4c9
HighW4y2H3ll <huzh@nyu.edu>
2021-10-27 21:50:33 +0000

    bump toolchains into x64 build, and ABI cxx11 disabled

--- tools/btrace/compile.sh
+++ tools/btrace/compile.sh
@@ -2,6 +2,6 @@
 dir=`dirname $0`
 cd $dir
 
-gcc -std=c99 -D_POSIX_SOURCE -D_GNU_SOURCE -fPIC -shared btrace.c btrace_linux.c -o libsw-btrace.so -m32
+gcc -std=c99 -D_POSIX_SOURCE -D_GNU_SOURCE -fPIC -shared btrace.c btrace_linux.c -o libsw-btrace.so
 mkdir -p ../libexec
 cp lib* ../libexec
