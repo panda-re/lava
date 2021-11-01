87b037a06e2108f175c57072776fb7078ae1ee62
zhenghaohu <huzh@nyu.edu>
2021-11-01 21:38:01 +0000

    bump toolchain into x64 && initial host-side docker

--- panda/setup.sh
+++ panda/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-LLVM_DIR="$HOME/bpf/install"
+#LLVM_DIR="$HOME/bpf/install"
+LLVM_DIR="/llvm-3.3-install"
 PANDA_DIR="$( realpath $( dirname "${BASH_SOURCE[0]}" ) )"
 
 mkdir -p ${PANDA_DIR}/build
 pushd ${PANDA_DIR}/build
-"../src/configure" \
+QEMU_CFLAGS='-D_GLIBCXX_USE_CXX11_ABI=0' CXXFLAGS='-D_GLIBCXX_USE_CXX11_ABI=0' "../src/configure" \
     --target-list=i386-softmmu \
     --prefix="${PANDA_DIR}/install" \
     --cc=gcc-6 --cxx=g++-6 \
