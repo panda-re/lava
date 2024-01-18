e8362e9319f48c566dbf8e7f47608195ec72a7d3
hu <huzh@nyu.edu>
2024-01-18 01:58:16 +0000

    update setup scripts

--- panda/setup.sh
+++ panda/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#LLVM_DIR="$HOME/bpf/install"
 LLVM_DIR="/llvm-3.3-install"
 PANDA_DIR="$( realpath $( dirname "${BASH_SOURCE[0]}" ) )"
 
@@ -11,7 +10,8 @@ QEMU_CFLAGS='-D_GLIBCXX_USE_CXX11_ABI=0' CXXFLAGS='-D_GLIBCXX_USE_CXX11_ABI=0' "
     --prefix="${PANDA_DIR}/install" \
     --cc=gcc-6 --cxx=g++-6 \
     --enable-llvm --with-llvm="${LLVM_DIR}" \
-    --python=python2
+    --python=python2 \
+    --extra-plugins-path="${PANDA_DIR}/.."
 
 make -j ${PANDA_NPROC:-$(nproc || sysctl -n hw.ncpu)}
 popd
